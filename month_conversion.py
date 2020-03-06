9dae532db5755dce651cdccdc282399b70bc4c83
Austin Atchley <austinatchley@gmail.com>
2020-03-06 13:47:45 -0600

    Add python script for converting 3 letter months to numeric form

---
+++ month_conversion.py
@@ -0,0 +1,28 @@
+import calendar
+import os
+import sys
+
+month_dict = dict((v.lower(),k) for k,v in enumerate(calendar.month_abbr))
+del month_dict['']
+print(month_dict)
+
+def convert_line(input_data):
+    if 'month' in input_data.lower():
+        for month, num in month_dict.iteritems():
+            index = input_data.find(month)
+            if index != -1:
+                input_data = input_data[:index] + str(num) + input_data[index+3:]
+
+    return input_data
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    input_file_split = os.path.splitext(input_file)
+
+    output_lines = list()
+    with open(input_file, "r") as f:
+        for line in f:
+            output_lines.append(convert_line(line))
+
+    output_file = open(input_file_split[0] + '.out' + input_file_split[1], "w+")
+    output_file.write(''.join(output_lines))
